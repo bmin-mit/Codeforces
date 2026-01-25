a7724f77d2f7e4474af584356969927602f417c1
Binh Minh <bmmit.work@gmail.com>
2026-01-25 23:53:39 +0700

    2178A - Yes or Yes

---
+++ 2178A_YesOrYes.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ss stringstream
+#define endl "\n"
+#define LLMAX INT64_MAX
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define MULTI \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+void solve()
+{
+    string s;
+    cin >> s;
+
+    cout << IF(count(s.begin(), s.end(), 'Y') <= 1, "YES", "NO") << endl;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    MULTI
+    solve();
+
+    return 0;
+}
