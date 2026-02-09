8775fb79c15d5d1b222ec036b9242373b56e5ebe
Binh Minh <bmmit.work@gmail.com>
2026-02-09 15:52:53 +0700

    1374C - Move Brackets

--- .template.cpp
+++ .template.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 #define int long long
-#define ss stringstream
 #define endl "\n"
-#define LLMAX INT64_MAX
+
 #define IF(cond, t, f) (cond ? t : f)
 #define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
 #define FAST_IO ios_base::sync_with_stdio(false), cin.tie(NULL)
@@ -15,7 +14,6 @@ using namespace std;
 
 void solve()
 {
-    
 }
 
 signed main()
