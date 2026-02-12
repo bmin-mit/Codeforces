97b82dec091d02bae847abd4bbcb57e56bb7efca
Binh Minh <bmmit.work@gmail.com>
2026-02-12 16:42:50 +0700

    560C - Gerald's Hexagon

---
+++ 560C_GeraldsHexagon.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), (void)cin.tie()
+#define MULTI \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+template <class T, class V>
+ostream &operator<<(ostream &o, pair<T, V> p)
+{
+    return o << "(" << p.first << ", " << p.second << ")";
+}
+
+#define P(T, O, C)                          \
+    template <class U>                      \
+    ostream &operator<<(ostream &o, T<U> v) \
+    {                                       \
+        o << O;                             \
+        int f = 0;                          \
+        for (auto &i : v)                   \
+            o << (f++ ? ", " : "") << i;    \
+        return o << C;                      \
+    }
+
+#define Q(T, O, C)                             \
+    template <class K, class V>                \
+    ostream &operator<<(ostream &o, T<K, V> v) \
+    {                                          \
+        o << O;                                \
+        int f = 0;                             \
+        for (auto &i : v)                      \
+            o << (f++ ? ", " : "") << i;       \
+        return o << C;                         \
+    }
+
+P(vector, '[', ']')
+P(set, '{', '}')
+Q(map, '{', '}')
+
+template <class T, size_t N>
+ostream &operator<<(ostream &o, array<T, N> v)
+{
+    o << "[";
+    int f = 0;
+    for (auto &i : v)
+        o << (f++ ? ", " : "") << i;
+    return o << "]";
+}
+
+void dbg(const char *s) { cerr << endl; }
+
+template <class T, class... A>
+void dbg(const char *s, T v, A... a)
+{
+    const char *c = s;
+    int b = 0;
+    while (*c && (*c != ',' || b))
+        b += bool(strchr("({[", *c)), b -= bool(strchr(")}]", *c)), c++;
+    cerr.write(s, c - s) << " = " << v << (sizeof...(a) ? " |" : "");
+    if (sizeof...(a))
+        dbg(c + 1, a...);
+    else
+        cerr << endl;
+}
+
+#ifdef LOCAL
+#define debug(...) dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+void solve()
+{
+    int top, topL, topR, bot, botL, botR;
+
+    cin >> top >> topR >> botR >> bot >> botL >> topL;
+    debug(top, topR, botR, bot, botL, topL);
+
+    int res = 0;
+    int width = (top - 1) * 2 + 1;
+    bool flag = false;
+    for (int i = 1; i <= topR + botR; ++i)
+    {
+        if (i <= topR && i <= topL)
+            width += 2;
+        else if (i > topR && i > topL)
+        {
+            if (flag)
+                width -= 2;
+            flag = true;
+        }
+        else
+        {
+            res += width + 1;
+            continue;
+        }
+        res += width;
+        debug(i, width, res);
+    }
+
+    cout << res;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    // MULTI
+    solve();
+
+    return 0;
+}
