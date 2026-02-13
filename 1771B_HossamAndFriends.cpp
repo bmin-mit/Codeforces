25efc784411352b1c6108c5845e87eb4a4c558f0
Binh Minh <bmmit.work@gmail.com>
2026-02-13 22:18:20 +0700

    1771B - Hossam and Friends

---
+++ 1771B_HossamAndFriends.cpp
@@ -0,0 +1,117 @@
+//* 362786689	Feb/13/2026 22:17UTC+7	Minh4893IT	B - Hossam and Friends	C++17 (GCC 7-32)	Accepted	78 ms	100 KB
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), (void)cin.tie()
+#define MULTI \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+template <class T, class V>
+ostream &operator<<(ostream &o, pair<T, V> p);
+
+#define P(T, O, C)                          \
+    template <class U>                      \
+    ostream &operator<<(ostream &o, T<U> v) \
+    {                                       \
+        o << O;                             \
+        int f = 0;                          \
+        for (auto &i : v)                   \
+            o << (f++ ? ", " : "") << i;    \
+        return o << C;                      \
+    }
+
+#define Q(T, O, C)                             \
+    template <class K, class V>                \
+    ostream &operator<<(ostream &o, T<K, V> v) \
+    {                                          \
+        o << O;                                \
+        int f = 0;                             \
+        for (auto &i : v)                      \
+            o << (f++ ? ", " : "") << i;       \
+        return o << C;                         \
+    }
+
+P(vector, '[', ']')
+P(set, '{', '}')
+Q(map, '{', '}')
+
+template <class T, class V>
+ostream &operator<<(ostream &o, pair<T, V> p)
+{
+    return o << "(" << p.first << ", " << p.second << ")";
+}
+
+template <class T, size_t N>
+ostream &operator<<(ostream &o, array<T, N> v)
+{
+    o << "[";
+    int f = 0;
+    for (auto &i : v)
+        o << (f++ ? ", " : "") << i;
+    return o << "]";
+}
+
+void dbg(const char *s) { cerr << endl; }
+
+template <class T, class... A>
+void dbg(const char *s, T v, A... a)
+{
+    const char *c = s;
+    int b = 0;
+    while (*c && (*c != ',' || b))
+        b += bool(strchr("({[", *c)), b -= bool(strchr(")}]", *c)), c++;
+    cerr.write(s, c - s) << " = " << v << (sizeof...(a) ? " |" : "");
+    if (sizeof...(a))
+        dbg(c + 1, a...);
+    else
+        cerr << endl;
+}
+
+#ifdef LOCAL
+#define debug(...) dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> notFr(n + 1, n + 1);
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        if (v > u)
+            notFr[u] = min(notFr[u], v);
+        else
+            notFr[v] = min(notFr[v], u);
+    }
+
+    for (int i = n - 1; i >= 1; i--)
+        notFr[i] = min(notFr[i], notFr[i + 1]);
+
+    int res = 0;
+    for (int i = 1; i <= n; ++i)
+        res += notFr[i] - i;
+    cout << res << endl;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    MULTI
+    solve();
+
+    return 0;
+}
